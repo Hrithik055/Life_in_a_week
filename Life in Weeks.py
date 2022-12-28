a508350e01584b868a20bd65969bbf6e6e6df582
Hrithik Kumar Sharma <119428353+Hrithik055@users.noreply.github.com>
2022-12-28 06:01:36 +0530

    Add files via upload

---
+++ Life in Weeks.py
@@ -0,0 +1,15 @@
+# 🚨 Don't change the code below 👇
+age = input("What is your current age? ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+time= 90 - int(age)
+x = 365 * int(time)
+y = 52 * int(time)
+z = 12 * int(time)
+
+print(f"You have {x} days, {y} weeks, and {z} months left")
+
+
+
+
